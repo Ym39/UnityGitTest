b3ccda3db4b486ba90d1f0ce05aa1a20bac5b566
Ym39 <dpals39@gmail.com>
2021-01-14 17:00:55 +0900

    fisrt 변수 추가

--- Assets/Script/Test.cs
+++ Assets/Script/Test.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    private int first; //first 브랜치에서 추가한 변수
+
     // Start is called before the first frame update
     void Start()
     {
